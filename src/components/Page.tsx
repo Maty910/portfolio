12bc1aacb3b9b7447e95166a50ace90fbd8c8293
Maty910 <matychacong@gmail.com>
2026-01-05 01:38:20 -0300

    fix: imports for deploy

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, type PropsWithChildren } from 'react';
 import { FiCode } from 'react-icons/fi';
-import { ExternalLink, Github, Terminal, Cpu, Database, Download, Mail } from 'lucide-react';
+import { ExternalLink, Github, Terminal, Cpu, Database, Mail } from 'lucide-react';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import type { Section } from '../types';
 
