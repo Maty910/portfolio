eb8b55135ab6b6bd1648e76e2ab0861406f4deac
Maty910 <matychacong@gmail.com>
2026-01-19 22:21:47 -0300

    feat: add projects images for slider

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -2,7 +2,12 @@
 export default [
   {
     slug: "p1",
-    image: "./images/DF Portfolio 1.png",
+    image: [
+      "./images/DF Portfolio 1.png",
+      "./images/DF Portfolio 2.png",
+      "./images/DF Portfolio 3.png",
+      "./images/DF Portfolio 4.png",
+    ],
     technologies: ["React", "Tailwind", "TypeScript"],
     githubUrl: "https://github.com/Maty910/Portafolio-Marra",
     liveUrl: "https://joaquinmarraccini.netlify.app/",
@@ -10,36 +15,49 @@ export default [
   },
   {
     slug: "p2",
-    image: "./images/project2.png",
+    image: [
+      "./images/SC 1.png",
+      "./images/SC 2.png",
+      "./images/SC 3.png",
+      "./images/SC 4.png"
+    ],
+    technologies: ["React", "Tailwind", "TypeScript"],
+    githubUrl: "https://github.com/Maty910/sachito-refrigeracion",
+    liveUrl: "https://sachitoref.netlify.app"
+  },
+  {
+    slug: "p3",
+    image: [
+      "./images/HN 1.png",
+      "./images/HN 2.png",
+      "./images/HN 3.png"
+    ],
+    technologies: ["TypeScript", "Tailwind", "Docker", "Node.js", "Express", "PostgreSQL"],
+    githubUrl: "https://github.com/Maty910/proyecto-dominio",
+    liveUrl: "https://hotel-now-poso.onrender.com/"
+  },
+  {
+    slug: "p4",
+    image: [
+      "./images/TL 1.png",
+      "./images/TL 2.png",
+      "./images/TL 3.png",
+    ],
     technologies: ["React", "Node.js", "SQLite", "Express"],
     githubUrl: "https://github.com/Maty910/tasklist",
     liveUrl: "https://tasklistmaty.netlify.app/"
   },
   {
-    slug: "p3",
+    slug: "p5",
     image: "./images/project3.png",
     technologies: ["React", "CSS"],
     githubUrl: "https://github.com/Maty910/Portafolio-Lu",
     liveUrl: "https://luciacastro.netlify.app/"
   },
   {
-    slug: "p4",
+    slug: "p6",
     image: "./images/project4.png",
     technologies: ["Python", "SQLite"],
     githubUrl: "https://github.com/Maty910/inventory"
   },
-  {
-    slug: "p5",
-    image: "./images/SC 1.png",
-    technologies: ["React", "Tailwind", "TypeScript"],
-    githubUrl: "https://github.com/Maty910/sachito-refrigeracion",
-    liveUrl: "https://sachitoref.netlify.app"
-  },
-  {
-    slug: "p6",
-    image: "./images/HN 1.png",
-    technologies: ["HTML", "CSS", "JavaScript"],
-    githubUrl: "https://github.com/Maty910/proyecto-dominio",
-    liveUrl: "https://hotelnow-project.netlify.app/"
-  }
 ];
