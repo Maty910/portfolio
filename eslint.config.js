eee07340d1df91ea81426cda49da0cec27f204be
Maty910 <matychacong@gmail.com>
2026-02-10 13:16:08 -0300

    chore: reformat ESLint config with consistent double quotes and imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,46 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
     ignores: [
-      'dist',
-      'dist/',
-      'build',
-      'build/',
-      'node_modules',
-      'node_modules/',
-      'coverage',
-      'coverage/',
-      '.vercel',
-      '.vite',
-      '*.config.js',
-      '**/*.config.js',
+      "dist",
+      "dist/",
+      "build",
+      "build/",
+      "node_modules",
+      "node_modules/",
+      "coverage",
+      "coverage/",
+      ".vercel",
+      ".vite",
+      "*.config.js",
+      "**/*.config.js",
     ],
   },
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-    ],
+    files: ["**/*.{ts,tsx}"],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     },
-  },
-)
+  }
+);
